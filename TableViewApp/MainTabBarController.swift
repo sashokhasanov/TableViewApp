b5a3ededdb3b59e7c8f42108d8040377ae6b35bd
Aleksandr Khasanov <sashokhasanov@mail.ru>
2021-12-28 12:59:08 +0300

    - table view controllers for plain list and sectioned list. - pass data from tab bar controller to table view controllers

---
+++ TableViewApp/MainTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabBarController.swift
+//  TableViewApp
+//
+//  Created by Сашок on 28.12.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    private var persons = Person.getPersonsList()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for viewController in viewControllers ?? [] {
+            let tabController = (viewController as? UINavigationController)?.topViewController
+            
+            if let plainListController = tabController as? PlainListViewController {
+                plainListController.persons = persons
+            } else if let sectionedListController = tabController as? SectionedListViewController {
+                sectionedListController.persons = persons
+            }
+            
+        }
+    }
+}
