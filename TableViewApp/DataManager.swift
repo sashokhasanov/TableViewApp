d561db4040039eed58f3d85a4214668997f0fe8a
Aleksandr Khasanov <sashokhasanov@mail.ru>
2021-12-28 12:45:00 +0300

    model layer implemented

---
+++ TableViewApp/DataManager.swift
@@ -0,0 +1,30 @@
+//
+//  DataManager.swift
+//  TableViewApp
+//
+//  Created by Сашок on 28.12.2021.
+//
+
+import Foundation
+
+class DataManager {
+    
+    static let instance = DataManager()
+    
+    let names = ["Jonh", "Mike", "Matt", "David",
+                 "Peter", "Paul", "Bill", "Richard", "Simon", "Jim"]
+    
+    let surnames = ["Stevenson", "Johnson", "Lumier",
+                    "Murphy", "Smith", "Taylor", "White", "Deeeze", "Garcia", "Miller"]
+    
+    let phones = ["(455) 942-2254", "(594) 278-0171", "(481) 409-4874",
+                  "(676) 912-0642", "(873) 804-2621", "(795) 601-6607",
+                  "(618) 827-4799", "(242) 421-3602", "(418) 768-1495", "(224) 886-4953"]
+    
+    let emails = ["wortmanj@verizon.net", "uncled@comcast.net", "dodong@yahoo.ca",
+                  "jgwang@sbcglobal.net", "mhassel@att.net","staffelb@att.net",
+                  "ideguy@sbcglobal.net", "houle@aol.com", "koudas@optonline.net", "mpiotr@live.com"]
+    
+    private init() {}
+    
+}
---
+++ TableViewApp/Person.swift
@@ -0,0 +1,35 @@
+//
+//  Person.swift
+//  TableViewApp
+//
+//  Created by Сашок on 28.12.2021.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    static func getPersonsList() -> [Person] {
+
+        let shuffledNames = DataManager.instance.names.shuffled()
+        let shuffledSurnames = DataManager.instance.surnames.shuffled()
+        let shuffledEmails = DataManager.instance.emails.shuffled()
+        let shuffledPhones = DataManager.instance.phones.shuffled()
+        
+        var result = [Person]()
+        result.reserveCapacity(shuffledNames.count)
+        
+        for index in 0..<shuffledNames.count {
+            result.append(Person(name: shuffledNames[index],
+                                 surname: shuffledSurnames[index],
+                                 phone: shuffledPhones[index],
+                                 email: shuffledEmails[index]))
+        }
+        
+        return result
+    }
+}
